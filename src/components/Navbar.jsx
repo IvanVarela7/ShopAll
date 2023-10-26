db8e48b46c99da788845e0fb4c3a68ae838bb9f2
IvanVarela7 <71855717+IvanVarela7@users.noreply.github.com>
2023-10-26 10:47:04 -0300

    navbar jsx y css V1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 function Navbar() {
-	return (
-		<div>Navbar</div>
-	)
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Logo
+          </Link>
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <Link to="/inicio" className="nav-links">
+                Inicio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/servicios" className="nav-links">
+                Servicios
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contacto" className="nav-links">
+                Contacto
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
