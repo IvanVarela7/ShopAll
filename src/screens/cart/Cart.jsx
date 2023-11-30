beb933a58d72a97e277299ad28a7aa2c73feef99
IvanVarela7 <hivanvarela@gmail.com>
2023-11-30 17:14:17 -0300

    se modifico la screen Cart para cuando el carrito esta vacio se muestr un mensaje

--- src/screens/cart/Cart.jsx
+++ src/screens/cart/Cart.jsx
@@ -1,18 +1,13 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { CartContext } from "../../context/CartContext";
 import OrderForm from "../../components/order_form/OrderForm";
 import CardCart from "../../components/card_cart/CardCart";
 import "./Cart.css";
 
-
 function Cart() {
-  const { cart, cartItems, fetchCartItems, emptyCart, removeItem } =
-    useContext(CartContext);
-
-  console.log(cartItems);
+  const { cart, cartItems, fetchCartItems, emptyCart, removeItem } = useContext(CartContext);
 
   useEffect(() => {
     if (cart.length > 0) {
@@ -20,32 +15,42 @@ function Cart() {
     }
   }, []);
 
+  // Verificar si el localStorage está vacío
+  const isLocalStorageEmpty = cartItems.length === 0;
+
   return (
     <>
       <Navbar />
-      <div className="cart-conteiner">
-        <div className="cart-productos">
-          {cartItems.map((item) => (
-            <CardCart
-              marca={item.marca}
-              descripcion={item.descripcion}
-              color={item.color}
-              talle={item.talle}
-              cantidad={item.cantidad}
-              precio={item.precio}
-              genero={item.genero}
-              imagen={item.imagen}
-              remover = {removeItem}
-            />
-          ))}
-        </div>
-        <div orders-conteiner>
-          <OrderForm />
-        </div>
-        <button onClick={emptyCart} className="vaciar-carrito-btn">Vaciar carrito</button>
-
+      <div className="cart-container">
+        {isLocalStorageEmpty ? (
+          <div className="empty-cart-message">El carrito está vacío</div>
+        ) : (
+          <>
+            <div className="cart-productos">
+              {cartItems.map((item) => (
+                <CardCart
+                  key={item.id} // Asegúrate de tener una key única para cada CardCart
+                  marca={item.marca}
+                  descripcion={item.descripcion}
+                  color={item.color}
+                  talle={item.talle}
+                  cantidad={item.cantidad}
+                  precio={item.precio}
+                  genero={item.genero}
+                  imagen={item.imagen}
+                  remover={removeItem}
+                />
+              ))}
+            </div>
+            <div className="orders-container">
+              <OrderForm />
+            </div>
+            <button onClick={emptyCart} className="vaciar-carrito-btn">
+              Vaciar carrito
+            </button>
+          </>
+        )}
       </div>
-
       <Footer />
     </>
   );
