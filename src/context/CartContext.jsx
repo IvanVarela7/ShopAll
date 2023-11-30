992e102b7491dd9751ed3e12d0a8fd046995a05c
IvanVarela7 <hivanvarela@gmail.com>
2023-11-30 17:22:55 -0300

    se mejoro la logica para el bug de agregar el mosmo producto

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,20 +1,13 @@
-import { Children, createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 
-// Crear el Contexto
 export const CartContext = createContext();
 
-// Crear el Provider para poder proveer el cotexto a la aplicacion
 export function CartProvider({ children }) {
-  // recuperamos datos del localStorage
-
   const storedItems = JSON.parse(localStorage.getItem("cartItems"));
   const initialItems = storedItems ? storedItems : [];
 
-  // aca va la info del contexto
-
   const [cart, setCart] = useState(initialItems);
-
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
@@ -23,16 +16,16 @@ export function CartProvider({ children }) {
   }, [cart]);
 
   const isInCart = (idProducto) =>
-    cart.find((item) => (item.id === idProducto ? true : false));
+    cart.some((item) => item.id === idProducto);
 
   const addItem = (idProducto, cantidad) => {
     if (isInCart(idProducto)) {
       setCart(
-        cart.map((item) => {
+        cart.map((item) =>
           item.id === idProducto
-            ? { id: idProducto, cantidad: item.cantidad + cantidad }
-            : item;
-        })
+            ? { ...item, cantidad: item.cantidad + cantidad }
+            : item
+        )
       );
     } else {
       setCart([...cart, { id: idProducto, cantidad: cantidad }]);
@@ -45,12 +38,9 @@ export function CartProvider({ children }) {
     try {
       const promises = cart.map(async (item) => {
         const itemRef = doc(db, "moda", item.id);
-
         const res = await getDoc(itemRef);
 
         if (res.exists()) {
-          const getQuantity = cart.find((item) => item.id === res.id);
-
           return { id: res.id, cantidad: item.cantidad, ...res.data() };
         }
       });
@@ -69,7 +59,7 @@ export function CartProvider({ children }) {
 
   const emptyCart = () => {
     setCart([]);
-    setCartItems([])
+    setCartItems([]);
   };
 
   const removeItem = (idProducto) =>{
